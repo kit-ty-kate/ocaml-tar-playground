b8883a2b3d54040a943f268ae10af710ce9aafed
Kate <kit-ty-kate@outlook.com>
2025-02-24 17:51:35 +0000

    Upgrade to ocaml-tar 3.0

--- test.ml
+++ test.ml
@@ -1,35 +1,51 @@
-module Tar_gz = Tar_gz.Make
-  (struct type 'a t = 'a
-          let ( >>= ) x f = f x
-          let return x = x end)
-  (struct type out_channel = Stdlib.out_channel
-          type 'a io = 'a
-          let really_write = Stdlib.output_string end)
-  (struct type in_channel = Stdlib.in_channel
-          type 'a io = 'a
-          let read ic buf = Stdlib.input ic buf 0 (Bytes.length buf) end)
+let ( let* ) = Tar.( let* )
 
-let print ic hdr =
-  let buf = Bytes.create (Int64.to_int hdr.Tar.Header.file_size) in
-  Tar_gz.really_read ic buf;
-  print_endline (Bytes.unsafe_to_string buf)
+let rec safe f a =
+  try f a with
+  | Unix.Unix_error (Unix.EINTR, _, _) -> safe f a
+
+let run t fd =
+  let rec run : type a. (a, _, _) Tar.t -> a = function
+    | Tar.Write str ->
+      ignore (safe (Unix.write_substring fd str 0) (String.length str))
+    | Tar.Read len ->
+      let b = Bytes.create len in
+      let read = safe (Unix.read fd b 0) len in
+      if read = 0 then
+        failwith "Unexpected_end_of_file"
+      else if len = (read : int) then
+        Bytes.unsafe_to_string b
+      else
+        Bytes.sub_string b 0 read
+    | Tar.Really_read len ->
+      let rec loop fd buf offset len =
+        if offset < (len : int) then
+          let n = safe (Unix.read fd buf offset) (len - offset) in
+          if n = 0 then
+            failwith "Unexpected_end_of_file"
+          else
+            loop fd buf (offset + n) len
+      in
+      let buf = Bytes.create len in
+      loop fd buf 0 len;
+      Bytes.unsafe_to_string buf
+    | Tar.Seek len -> ignore (safe (Unix.lseek fd len) Unix.SEEK_CUR)
+    | Tar.Return (Ok x) -> x
+    | Tar.Return (Error _) -> failwith "something's gone wrong"
+    | Tar.High _ -> assert false
+    | Tar.Bind (x, f) -> run (f (run x))
+  in
+  run t
+
+let list fd =
+  let go ?global:_ hdr () =
+    let* content = Tar.really_read (Int64.to_int hdr.Tar.Header.file_size) in
+    print_endline content;
+    Tar.return (Ok ())
+  in
+  run (Tar_gz.in_gzipped (Tar.fold go ())) fd
 
 let () =
-  let ic = open_in Sys.argv.(1) in
-  let ic = Tar_gz.of_in_channel ~internal:(De.bigstring_create De.io_buffer_size) ic in
-  let rec go global = match Tar_gz.HeaderReader.read ~global ic with
-    | Ok (hdr, global) ->
-        let data_length =
-          if String.equal hdr.Tar.Header.file_name Sys.argv.(2) then begin
-            print ic hdr;
-            0 (* Data already read *)
-          end else
-            Int64.to_int hdr.Tar.Header.file_size
-        in
-        let data_padding = Tar.Header.compute_zero_padding_length hdr in
-        Tar_gz.skip ic (data_length + data_padding);
-        go global
-    | Error (`Fatal _) -> failwith "malformed tar.gz file"
-    | Error `Eof -> ()
-  in
-  go None
+  let filename = Sys.argv.(1) in
+  let fd = Unix.openfile filename [ Unix.O_RDONLY ] 0 in
+  list fd
